61f107c556b1d975e7090777cf48d2b970812e74
Paulo Campos <paulo4-0@hotmail.com>
2024-02-29 23:00:43 -0300

    create route to get unique poll

--- src/http/server.ts
+++ src/http/server.ts
@@ -1,9 +1,11 @@
 import fastify from 'fastify'
 import { createPoll } from './routes/createPoll'
+import { getPoll } from './routes/getPoll'
 
 const app = fastify()
 
 void app.register(createPoll)
+void app.register(getPoll)
 
 void app.listen({ port: 3333 }).then(() => {
   console.log('Server Run')
