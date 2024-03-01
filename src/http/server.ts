413fb680442ffedfbb35f75b3edbfcc88209568a
Paulo Campos <paulo4-0@hotmail.com>
2024-02-29 23:48:13 -0300

    create vote route and set cookies

--- src/http/server.ts
+++ src/http/server.ts
@@ -1,12 +1,19 @@
 import fastify from 'fastify'
 import { createPoll } from './routes/createPoll'
 import { getPoll } from './routes/getPoll'
+import { voteOnPoll } from './routes/voteOnPolls'
+import cookie from '@fastify/cookie'
 
 const app = fastify()
 
-void app.register(createPoll)
-void app.register(getPoll)
+app.register(cookie, {
+  secret: 'e81745c7-9f92-4c77-990f-592831ae4f2e',
+  hook: 'onRequest'
+})
+app.register(createPoll)
+app.register(getPoll)
+app.register(voteOnPoll)
 
-void app.listen({ port: 3333 }).then(() => {
+app.listen({ port: 3333 }).then(() => {
   console.log('Server Run')
 })
