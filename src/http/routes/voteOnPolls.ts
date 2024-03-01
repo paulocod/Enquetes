4df9bba8332551047afeb7502c06906f1334c735
Paulo Campos <paulo4-0@hotmail.com>
2024-03-01 07:46:39 -0300

    create a zrank in redis when the vote on poll is created

--- src/http/routes/voteOnPolls.ts
+++ src/http/routes/voteOnPolls.ts
@@ -2,6 +2,7 @@ import { type FastifyInstance } from 'fastify'
 import { prisma } from '../../lib/prisma'
 import { z } from 'zod'
 import { randomUUID } from 'node:crypto'
+import { redis } from '../../lib/redis'
 
 export async function voteOnPoll (app: FastifyInstance): Promise<void> {
   app.post('/polls/:pollId/votes', async (request, reply) => {
@@ -31,6 +32,8 @@ export async function voteOnPoll (app: FastifyInstance): Promise<void> {
             id: userPreviousVoteOnPoll.id
           }
         })
+
+        await redis.zincrby(pollId, -1, userPreviousVoteOnPoll.pollOptionId)
       } else if (userPreviousVoteOnPoll) {
         return await reply.status(400).send({ message: 'You already voted on this poll.' })
       }
@@ -54,6 +57,8 @@ export async function voteOnPoll (app: FastifyInstance): Promise<void> {
         pollOptionId
       }
     })
+
+    await redis.zincrby(pollId, 1, pollOptionId)
 
     return await reply.status(201).send()
   })
