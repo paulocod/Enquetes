daf4d34774ae6252612177d765eab54bd3dce9c5
Paulo Campos <paulo4-0@hotmail.com>
2024-03-01 22:25:47 -0300

    register route

--- src/http/server.ts
+++ src/http/server.ts
@@ -3,6 +3,8 @@ import { createPoll } from './routes/createPoll'
 import { getPoll } from './routes/getPoll'
 import { voteOnPoll } from './routes/voteOnPolls'
 import cookie from '@fastify/cookie'
+import websocket from '@fastify/websocket'
+import { pollResults } from './ws/pollResult'
 
 const app = fastify()
 
@@ -10,9 +12,13 @@ app.register(cookie, {
   secret: 'e81745c7-9f92-4c77-990f-592831ae4f2e',
   hook: 'onRequest'
 })
+
+app.register(websocket)
+
 app.register(createPoll)
 app.register(getPoll)
 app.register(voteOnPoll)
+app.register(pollResults)
 
 app.listen({ port: 3333 }).then(() => {
   console.log('Server Run')
